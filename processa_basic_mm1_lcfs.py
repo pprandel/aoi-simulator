9562e2e56eaef3f9d6a777d9ebd3d3cc943a6597
Paulo Prandel <prandel.paulo@eb.mil.br>
2021-06-18 11:53:40 -0300

    Mean AoI LCFS

---
+++ processa_basic_mm1_lcfs.py
@@ -0,0 +1,24 @@
+import numpy as np
+from mean_aoi import mean_aoi
+
+RO = [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
+ARRIVAL_TIMES = [1/r for r in RO]
+
+def calc_analitic_aoi(ro, mu):
+    return (1/mu) * (1 + 1/ro)
+
+sim_aoi = {}
+analit_aoi = {}
+
+for ro in RO:
+    print("Processando mm1_ro_%s" %(str(ro)))
+    path = "experimentos/lcfs_preemp_sv/mm1_ro_" + str(ro) + ".txt"
+    sim_aoi[ro] = mean_aoi(path)
+    analit_aoi[ro] = calc_analitic_aoi(ro, 1)
+
+with open("resultados/sim_mm1_lcfs_aoi.txt", 'w') as f:
+    f.write(str(sim_aoi))
+
+with open("resultados/analit_mm1_lcfs_aoi.txt", 'w') as f:
+    f.write(str(analit_aoi))
+
