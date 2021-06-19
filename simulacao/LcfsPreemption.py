7d6e64fb7637d86ed43618d207cf9a9062878c0f
Paulo Cesar Prandel <prandel.paulo@eb.mil.br>
2021-06-19 13:19:57 -0300

    Separa a classe LcfsPreemption

--- simulacao/LcfsPreemption.py
+++ simulacao/LcfsPreemption.py
@@ -1,9 +1,10 @@
-import queueing_tool as qt
-import numpy as np
 from numpy import infty
-from heapq import heappush, heappop, nlargest
+import numpy
+from heapq import heappush, heappop
+import queueing_tool as qt
 
 """
+Extends QueueServer Class from queueing_tool package
 Last Come First Served Queue with finite capacity and preemption
 
 Parameters
@@ -85,24 +86,3 @@ class LcfsPreemption(qt.QueueServer):
                 arrival.queue_action(self, 1)
                 arrival._time = self.service_f(arrival._time)
                 self._update_time()
-
-
-RO = list(np.arange(0.1, 2, 0.1))
-ARRIVAL_TIMES = [1/r for r in RO]
-
-mean_delay = {}
-
-for i, arr_time in enumerate(ARRIVAL_TIMES):
-    print(arr_time)
-    def arr(t): return t+ np.random.exponential(arr_time)
-    def ser(t): return t + 1
-    q = LcfsPreemption(preemption=0, num_servers=1, arrival_f=arr, service_f=ser)
-    q.clear()
-    q.set_active()
-    q.collect_data = True
-    q.simulate(n=10000, nD=10000)
-    num_events = q.num_arrivals[0] + q.num_departures
-    data = q.data
-    arq_nome = "experimentos/md1_lcfs_s/md1_ro_" + str(RO[i]) + ".txt"
-    with open(arq_nome, 'w') as f:
-        f.write(str(data))
--- simulacao/simula_basic_mm1_lcfs_s.py
+++ simulacao/simula_basic_mm1_lcfs_s.py
@@ -1,8 +1,7 @@
-import queueing_tool as qt
+from LcfsPreemption import LcfsPreemption
 import numpy as np
 
-
-RO = [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
+RO = list(np.arange(0.1, 2, 0.1))
 ARRIVAL_TIMES = [1/r for r in RO]
 
 mean_delay = {}
@@ -11,13 +10,13 @@ for i, arr_time in enumerate(ARRIVAL_TIMES):
     print(arr_time)
     def arr(t): return t+ np.random.exponential(arr_time)
     def ser(t): return t + np.random.exponential(1)
-    q = qt.QueueServer(1, arrival_f=arr, service_f=ser)
+    q = LcfsPreemption(preemption=0, num_servers=1, arrival_f=arr, service_f=ser)
     q.clear()
     q.set_active()
     q.collect_data = True
     q.simulate(n=10000, nD=10000)
     num_events = q.num_arrivals[0] + q.num_departures
     data = q.data
-    arq_nome = "experimentos/mm1_ro_" + str(RO[i]) + ".txt"
+    arq_nome = "experimentos/mm1_lcfs_s/mm1_ro_" + str(RO[i]) + ".txt"
     with open(arq_nome, 'w') as f:
         f.write(str(data))
