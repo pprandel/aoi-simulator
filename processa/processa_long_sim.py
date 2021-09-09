b040de11ad52078b285d67bed227674c6cbecbc3
Paulo Prandel <prandel.paulo@eb.mil.br>
2021-09-09 16:37:02 -0300

    Autocorrelation analysis

--- processa/processa_long_sim.py
+++ processa/processa_long_sim.py
@@ -2,7 +2,7 @@ import numpy as np
 import ast, json
 
 #RO = [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
-ro = 0.5
+ro = 0.95
 mu = 1
 # ARRIVAL_TIMES = [1/r for r in RO]
 arr_time = 1/(mu*ro)
@@ -36,7 +36,7 @@ def calc_aoi(data):
         Qi_atual = Qi(Ti, Yi)
         Qi_total = Qi_total + Qi_atual
         age_media = Qi_total/(ti_linha-t_inicio)
-        Q_vector.append(age_media)
+        Q_vector.append(Qi_atual)
         V_total = V_total + (Qi_atual - Qi_total/(i+1))**2
         erro = V_total/((ti_linha-t_inicio)**(2))
         V_vector.append(erro)
