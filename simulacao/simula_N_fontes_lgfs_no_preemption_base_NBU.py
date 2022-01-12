e243a8a7b86c78c4b80f40e2e4b1b71656bcdffb
Paulo Cesar Prandel <prandel.paulo@eb.mil.br>
2022-01-12 18:51:38 -0300

    Ajuste no tempo de chegada

--- simulacao/simula_N_fontes_lgfs_no_preemption_base_NBU.py
+++ simulacao/simula_N_fontes_lgfs_no_preemption_base_NBU.py
@@ -13,7 +13,7 @@ sim_name = "mm1_N_sources_lgfs_no_preemption_NBU"
 # Create adjacency matrix
 # Each edge represents a queue and must have a type
 adjacency = {}
-ro = 0.95
+ro = 0.8
 N = 50
 num_servers = 3
 for i in range(N):
@@ -28,16 +28,20 @@ q_cl = {1: AoiQueueServer, 2: LgfsMultiServerNoPreemption}
 
 # Define packet generation and service functions
 # Queue service rate
-mu = 1 #/0.886
+mu = 1 
 # Packet generation rates
 lamb = (ro * mu * num_servers) / N
 
 # Poisson generation queues (exponential interarrival times)
 def f_gen_1(t): return t + np.random.exponential(1/lamb)
 # Instant service queue
-def f_ser_1(t): return t
+def f_ser_1(t): 
+    if np.random.random() < 0.5:
+        return t
+    else:
+        return t + 4 / lamb
 
-def f_ser_2(t): return t + expon.rvs(loc=1/3, scale=2/3)#np.random.weibull(2)
+def f_ser_2(t): return t + expon.rvs(loc=1/3, scale=2/3) #np.random.weibull(2)
 
 # Config queues parameters for each edge type
 q_ar = {
@@ -49,7 +53,7 @@ q_ar = {
     2: {
         'service_f': f_ser_2,
         'num_servers': num_servers,
-        'policy': 0
+        'policy': 2
     }
 }
 
